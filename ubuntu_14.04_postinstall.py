e4e4e06d4fb6e6eb1bcbe6e099cd5f174729f727
dane-lyon <dane-lyon@users.noreply.github.com>
2014-04-28 17:26:08 +0200

    ajout validation licence police MS

--- ubuntu_14.04_postinstall.py
+++ ubuntu_14.04_postinstall.py
@@ -285,6 +285,10 @@ def main(argv):
 	# Activation licence JAVA
 	showexec ("Activation licence Java...", "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections")
 
+	# Activation licence EULA pour les polices Microsoft
+	showexec ("Activation licence Polices MS...", "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo /usr/bin/debconf-set-selections")
+
+
 	# Parse and install packages
 	for pkg_type, pkg_list in config.items("packages"):
 		if (pkg_type.startswith("remove_")):
