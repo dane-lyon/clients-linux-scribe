7a78804dcecdd73644a40e01b9eff19a5e8139be
dane-lyon <dane-lyon@users.noreply.github.com>
2014-04-28 17:02:15 +0200

    Update ubuntu_14.04_postinstall.py

--- ubuntu_14.04_postinstall.py
+++ ubuntu_14.04_postinstall.py
@@ -337,16 +337,7 @@ def main(argv):
 	for pkg_type, pkg_list in config.items("debs"):
 		showexec ("Telechargement de"+pkg_type, _WGET+" -O /tmp/"+pkg_type)
 		showexec ("Installation du paquet "+pkg_type, _DPKG_INSTALL+" /tmp/"+pkg_type)
-		# TeamViewer
-	#	if (config.has_option("debs", "pkg_teamviewer")):
-	#		showexec ("Téléchargement de TeamViewer", _WGET+" -O /tmp/teamviewer.deb "+config.get("debs", "pkg_teamviewer"))
-	#		showexec ("Installation de TeamViewer", _DPKG_INSTALL+" /tmp/teamviewer.deb ")
-		# Skype
-	#	if (config.has_option("debs", "pkg_skype")):
-	#		showexec ("Téléchargement de Skype", _WGET+" -O /tmp/skype.deb "+config.get("debs", "pkg_skype"))
-	#		showexec ("Installation de Skype", _DPKG_INSTALL+" /tmp/skype.deb ")
-
-
+		
 
 	# Gnome 3 configuration
 	if (config.has_section("gnome3")):
