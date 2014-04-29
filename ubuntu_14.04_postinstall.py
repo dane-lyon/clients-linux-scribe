c8ee3a38d3455b99d81f93836ac54bf6ab2c6338
bristow <cedric.frayssinet@gmail.com>
2014-04-29 09:49:44 +0200

    Update ubuntu_14.04_postinstall.py

--- ubuntu_14.04_postinstall.py
+++ ubuntu_14.04_postinstall.py
@@ -326,21 +326,10 @@ def main(argv):
 		if (config.has_option("dotfiles", "htoprc")):
 			showexec ("Install the Htop configuration file", _WGET+" -O $HOME/.htoprc "+config.get("dotfiles", "htoprc"))
 
-	# Installation des paquets deb
-	#if (config.has_section("debs")):
-		# TeamViewer
-	#	if (config.has_option("debs", "pkg_teamviewer")):
-	#		showexec ("Téléchargement de TeamViewer", _WGET+" -O /tmp/teamviewer.deb "+config.get("debs", "pkg_teamviewer"))
-	#		showexec ("Installation de TeamViewer", _DPKG_INSTALL+" /tmp/teamviewer.deb ")
-		# Skype
-	#	if (config.has_option("debs", "pkg_skype")):
-	#		showexec ("Téléchargement de Skype", _WGET+" -O /tmp/skype.deb "+config.get("debs", "pkg_skype"))
-	#		showexec ("Installation de Skype", _DPKG_INSTALL+" /tmp/skype.deb ")
 
 	# Installation des paquets deb en installant ligne par ligne
 	for pkg_type, pkg_list in config.items("debs"):
 		showexec ("Telechargement de "+pkg_type, _WGET+" -O /tmp/"+pkg_type+".deb "+pkg_list)
-		#+config.get("debs", pkg_type))
 		showexec ("Installation du paquet "+pkg_type, _DPKG_INSTALL+" /tmp/"+pkg_type+".deb")
 		
 	# Gnome 3 configuration
