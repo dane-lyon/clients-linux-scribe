518405be11f27d132aaff70d1cd27611e02dd145
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-11-09 15:23:02 +0100

    Create xubuntu14.04_postinstall.sh

---
+++ xubuntu14.04_postinstall.sh
@@ -0,0 +1,12 @@
+# a faire
+
+[packages]
+ubuntu-restrictive = xubuntu-restricted-extras xubuntu-restricted-addons 
+multimedia = pinta imagination openshot audacity inkscape gthumb vlc x264 ffmpeg2theora oggvideotools mplayer hugin gimp ogmrip flac vorbis-tools lame mypaint libdvdread4
+systeme = gparted vim pyrenamer rar xfce4-goodies xfwm4-themes
+web = chromium-browser flashplugin-downloader ttf-mscorefonts-installer
+mathematiques = geogebra algobox carmetal
+sciences = stellarium celestia avogadro marble
+bureautique = libreoffice libreoffice-l10n-fr libreoffice-help-fr freeplane shutter scribus
+programmation = scratch idle-python2.7
+dep_googleearth = libfontconfig1:i386 libx11-6:i386 libxrender1:i386 libxext6:i386 libgl1-mesa-glx:i386 libglu1-mesa:i386 libglib2.0-0:i386 libsm6:i386
