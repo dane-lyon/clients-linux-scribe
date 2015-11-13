87836e07818c719f4e89c6b79cfaa4d659ee5312
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2015-11-13 16:44:20 +0100

    Update xubuntu14.04_postinstall.sh

--- xubuntu14.04_postinstall.sh
+++ xubuntu14.04_postinstall.sh
@@ -18,14 +18,17 @@ apt-get -y install xubuntu-restricted-extras xubuntu-restricted-addons xfce4-goo
 # si Lubuntu (LXDE), remplacer cette ligne par :
 #apt-get -y install lubuntu-restricted-extras lubuntu-restricted-addons
 
+#[[bureautique]]
+apt-get -y install ttf-mscorefonts-installer libreoffice libreoffice-l10n-fr libreoffice-help-fr freeplane scribus
+
+#[[web]]
+apt-get -y install firefox chromium-browser flashplugin-downloader pepperflashplugin-nonfree
+
 #[[multimedia]]
 apt-get -y install gimp pinta imagination openshot audacity inkscape gthumb vlc x264 ffmpeg2theora flac vorbis-tools lame mypaint libdvdread4
 
 #[[systeme]]
 apt-get -y install gparted vim pyrenamer unrar htop shutter
-
-#[[web]]
-apt-get -y install firefox chromium-browser flashplugin-downloader pepperflashplugin-nonfree 
 
 #[[mathematiques]]
 apt-get -y install geogebra algobox carmetal
@@ -33,19 +36,16 @@ apt-get -y install geogebra algobox carmetal
 #[[sciences]]
 apt-get -y install stellarium celestia avogadro
 
-#[[bureautique]]
-apt-get -y install libreoffice libreoffice-l10n-fr libreoffice-help-fr ttf-mscorefonts-installer freeplane scribus
-
 #[[programmation]]
 apt-get -y install scratch idle-python2.7
-
 
 #-----------------------------------------
 # Customisation graphique Xubuntu 
 #-----------------------------------------
 
 # A venir...
-
+# wget http://nux87.free.fr/xubuntu-custom/skel.tar.gz
+# tar ......................
 
 #-----------------------------------------
 # Fin
