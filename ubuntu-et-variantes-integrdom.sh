935fbd55633e73686df7a62ac3f65db0c9a11eed
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-03-24 15:12:01 +0100

    Update ubuntu-et-variantes-integrdom.sh

--- ubuntu-et-variantes-integrdom.sh
+++ ubuntu-et-variantes-integrdom.sh
@@ -289,7 +289,7 @@ fi
 if [ "$(which mdm)" = "/usr/sbin/mdm" ] ; then # si MDM est installé (donc Mint)
   apt-get -y purge mintwelcome hexchat pidgin transmission-gtk banshee
   cp /etc/mdm/mdm.conf /etc/mdm/mdm_old.conf
-  wget http://nux87.free.fr/ubuntu-script-dane/mint-integrdom/mdm.conf ; mv mdm.conf /etc/mdm/
+  wget http://dane.ac-lyon.fr/spip/IMG/zip/mdm.conf.zip ; unzip mdm.conf.zip -d /etc/mdm/ ; rm mdm.conf.zip ;
 fi
 
 ########################################################################
