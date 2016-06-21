b81f70b03ebba8f1415447f779d20b4d4f1613fe
Délégation Académique au Numérique pour l'Education de Lyon <dane-lyon@users.noreply.github.com>
2016-06-21 10:57:27 +0200

    Create skel_perso.sh

---
+++ skel_perso.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Ce script est là pour automatiser la copie du profil en cours dans le répertoire skel afin d'avoir un bureau customisé
+#homogène sur l'ensemble du parc. Vous devez donc avoir personnalisé le bureau comme vous le souhaitez avant d'appliquer
+#ce script. 
+
+# Doit impérativement être lancé avec sudo !
+
+#############################################
+# Run using sudo, of course.
+#############################################
+if [ "$UID" -ne "0" ]
+then
+  echo "Il faut lancer ce script avec sudo => sudo ./skelperso.sh"
+  exit 
+fi
+
+#############################################
+# Copie votre profil
+#############################################
+cp -rf ~/.config /etc/skel/ ;
+cp -rf ~/.local /etc/skel/ ;
+cp -rf ~/.gconf /etc/skel/ ;
+cp -rf ~/Bureau /etc/skel/ ;
+cp -rf ~/Desktop /etc/skel/ ;
+
+#############################################
+# Page par défaut Firefox (a décommenter si besoin)
+#############################################
+#echo "user_pref(\"browser.startup.homepage\", \"http://lite.qwant.com\");" >> /usr/lib/firefox/defaults/pref/channel-prefs.js
