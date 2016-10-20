370eafadf526e6dfe837e503e89147d605ab7b09
yvand <yvan.duron@gmail.com>
2016-10-20 20:43:49 +0200

    Amélioration lisibilité du code (syntaxe + uniforme notamment) + Corrections ortho

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Scripts pour Client Scribe (x)Ubuntu 14.04 & 16.04
 
-Ce **script** permet d'intégrer des clients Gnu/Linux dans un environnement Eole-Scribe 2.3, 2.4 ou 2.5 
+Ce **script** permet d'intégrer des clients GNU/Linux dans un environnement Eole-Scribe 2.3, 2.4 ou 2.5.
 
 Les clients supportés/testés avec les scripts sont les suivants :
 - Ubuntu (Environnement Unity) 14.04 et 16.04
@@ -12,28 +12,29 @@ Les clients supportés/testés avec les scripts sont les suivants :
 Avant de lancer ce script, assurez-vous d'avoir installé toutes vos applications, puis vous pouvez cloner vos postes
 avec la solution libre [OSCAR](http://oscar.crdp-lyon.fr/wiki/)
 
-  - Télécharger le script le script d'intégration :
-	
-	<code>wget https://raw.githubusercontent.com/dane-lyon/clients-linux-scribe/master/ubuntu-et-variantes-integrdom.sh</code>
+- Télécharger le script le script d'intégration :
+```bash
+wget https://raw.githubusercontent.com/dane-lyon/clients-linux-scribe/master/ubuntu-et-variantes-integrdom.sh
+```
 
-  - Se placer dans le répertoire courant puis lancer les commandes :
-	
-	<code>chmod +x ubuntu-et-variantes-integrdom.sh</code>
+- Se placer dans le répertoire courant puis lancer les commandes :
+```bash
+chmod +x ubuntu-et-variantes-integrdom.sh
+sudo ./ubuntu-et-variantes-integrdom.sh
+```
 
-	<code>sudo ./ubuntu-et-variantes-integrdom.sh</code>
-
-###**Remarques :** 
+### **Remarques :**
 
 #### Script de post-installation
 
-Pour gagner du temps lors de la création du poste modèle, on pourra utiliser ensuite le script de post-installation qui installera le système avec toutes les applications souhaitées : https://raw.githubusercontent.com/dane-lyon/clients-linux-scribe/master/ubuntu-et-variantes-postinstall.sh 
+Pour gagner du temps lors de la création du poste modèle, on pourra utiliser ensuite le script de post-installation qui installera le système avec toutes les applications souhaitées : https://raw.githubusercontent.com/dane-lyon/clients-linux-scribe/master/ubuntu-et-variantes-postinstall.sh
 
 #### Partages
 
 Si votre serveur Scribe est en version "2.4" ou "2.5", par défaut vous n'aurez pas les partages avec les clients linux (groupes, classes etc...), pour régler le problème, vous devez faire la manipulation suivante sur votre serveur :
 https://dane.ac-lyon.fr/spip/Client-Linux-activer-les-partages
 
-### Problèmes d'identifications possibles 
+### Problèmes d'identifications possibles
 
 Pour ne pas avoir de problème d'identification, vérifier que :
 
@@ -55,28 +56,32 @@ Vous pouvez éditer les valeurs par défaut en début de script afin de les adap
 
 Pour personnaliser le menu d'Unity à tous les utilisateurs, chercher dans le script ces lignes :
 
-	echo "[com.canonical.indicator.session]
-	user-show-menu=false
-	[org.gnome.desktop.lockdown]
-	disable-user-switching=true
-	disable-lock-screen=true
-	[com.canonical.Unity.Launcher]
-	favorites=[ 'nautilus-home.desktop', 'firefox.desktop','libreoffice-startcenter.desktop', 'gcalctool.desktop','gedit.desktop','gnome-screenshot.desktop' ]
-	" > /usr/share/glib-2.0/schemas/my-defaults.gschema.override
+```bash
+echo "[com.canonical.indicator.session]
+user-show-menu=false
+[org.gnome.desktop.lockdown]
+disable-user-switching=true
+disable-lock-screen=true
+[com.canonical.Unity.Launcher]
+favorites=[ 'nautilus-home.desktop', 'firefox.desktop', 'libreoffice-startcenter.desktop', 'gnome-calculator.desktop', 'gedit.desktop', 'org.gnome.Screenshot.desktop' ]
+" > /usr/share/glib-2.0/schemas/my-defaults.gschema.override
+```
 
 La ligne
-	favorites=[ 'nautilus-home.desktop', 'firefox.desktop','libreoffice-startcenter.desktop','gcalctool.desktop','gedit.desktop','gnome-screenshot.desktop' ]
+`favorites=[ 'nautilus-home.desktop', 'firefox.desktop','libreoffice-startcenter.desktop','gcalctool.desktop','gedit.desktop','gnome-screenshot.desktop' ]`
 est à adapter en fonction de vos besoins :
 
-Pour connaitre le nom des raccourcis, faire dans un terminal : ls /usr/share/applications/
+Pour connaitre le nom des raccourcis, faire dans un terminal : `ls /usr/share/applications/`
 
-Pour voir à quelle application cela correspond, avec l'explorateur, il faut se déplacer dans /usr/share/applications/
+Pour voir à quelle application cela correspond, avec l'explorateur, il faut se déplacer dans `/usr/share/applications/`
 
 Pour appliquer les modifications, il faut lancer la commande :
 
-	<code>sudo glib-compile-schemas /usr/share/glib-2.0/schemas</code>
+```bash
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+```
 
-A noter que chaque élève ou enseignant pourra personnaliser son menu par la suite puisque le profil est local. La personnalisation sera, en revanche, propre à chaque poste.
+À noter que chaque élève ou enseignant pourra personnaliser son menu par la suite puisque le profil est local. La personnalisation sera, en revanche, propre à chaque poste.
 
 ### TO DO :
 
--- ubuntu-et-variantes-integrdom.sh
+++ ubuntu-et-variantes-integrdom.sh
@@ -9,7 +9,7 @@
 # - Lubuntu 14.04 /LXDE
 # - Lubuntu 16.04 /LXDE
 # - Ubuntu Mate 16.04 /Mate
-# - Linux Mint 17.3/Cinnamon & Mate 
+# - Linux Mint 17.3/Cinnamon & Mate
 # - Linux Mint 18/Cinnamon, Mate & XFCE
 
 ###### Intégration client scribe 2.3/2.4/2.5 pour client basé sur (x)Ubuntu 14.04/16.04 ###### 
@@ -22,15 +22,15 @@
 
 ### changement apporté pour la version 14.04/16.04 :
 
-# - valeur de vérification 12.04 remplacé par 14.04 ainsi que 16.04 (+17/18 pour mint)
-# - paquet a installer smbfs remplacé par cifs-utils car il a changé de nom.
+# - valeur de vérification 12.04 remplacée par 14.04 ainsi que 16.04 (+17/18 pour mint)
+# - paquet à installer smbfs remplacé par cifs-utils car il a changé de nom.
 # - ajout groupe dialout
 # - désinstallation de certains logiciels inutiles suivant les variantes
 # - ajout fonction pour programmer l'extinction automatique des postes le soir
 # - lecture dvd inclus
 # - changement du thème MDM par défaut pour Mint (pour ne pas voir l'userlist)
 # - Ajout d'une ligne dans sudoers pour régler un problème avec GTK dans certains cas sur Ubuntu 14.04
-# - Changement page d'acceuil Firefox
+# - Changement page d'accueil Firefox
 
 #Christophe Deze - Rectorat de Nantes
 #Cédric Frayssinet - Mission Tice Ac-lyon
@@ -53,20 +53,18 @@ pagedemarragepardefaut="https://lite.qwant.com"
 #############################################
 # Run using sudo, of course.
 #############################################
-if [ "$UID" -ne "0" ]
-then
-  echo "Il faut etre root pour executer ce script. ==> sudo "
-  exit 
-fi 
+if [ "$UID" -ne "0" ] ; then
+  echo "Il faut être root pour exécuter ce script. ==> sudo"
+  exit 1
+fi
 
 ########################################################################
 #vérification de la bonne version d'Ubuntu
 ########################################################################
 . /etc/lsb-release
-if [ "$DISTRIB_RELEASE" != "14.04" ] && [ "$DISTRIB_RELEASE" != "17.3" ] && [ "$DISTRIB_RELEASE" != "16.04" ] && [ "$DISTRIB_RELEASE" != "18" ]
-then
-  echo "Vous n'êtes pas sûr une version compatible, rappel des versions supportés pour ce script : Ubuntu & Variante 14.04/16.04, Linux Mint 17.3/18"
-  exit
+if [ "$DISTRIB_RELEASE" != "14.04" ] && [ "$DISTRIB_RELEASE" != "17.3" ] && [ "$DISTRIB_RELEASE" != "16.04" ] && [ "$DISTRIB_RELEASE" != "18" ] ; then
+  echo "Vous n'êtes pas sur une version compatible, rappel des versions supportées pour ce script : Ubuntu & Variante 14.04/16.04, Linux Mint 17.3/18"
+  exit 1
 fi
 
 ##############################################################################
@@ -74,7 +72,7 @@ fi
 ##############################################################################
 read -p "Donnez l'ip du serveur Scribe ? [$scribe_def_ip] " ip_scribe
 if [ "$ip_scribe" = "" ] ; then
- ip_scribe=$scribe_def_ip
+  ip_scribe=$scribe_def_ip
 fi
 echo "Adresse du serveur Scribe = $ip_scribe"
 
@@ -98,7 +96,7 @@ else
 fi
 
 ###################################################
-# cron d'extinction automatique a lancer ?
+# cron d'extinction automatique à lancer ?
 ###################################################
 
 echo "Pour terminer, voulez vous activer l'extinction automatique des postes le soir ?"
@@ -109,13 +107,11 @@ echo "3 = oui, extinction a 22H00"
 read -p "Répondre par le chiffre correspondant (0,1,2,3) : " rep_proghalt
 
 if [ "$rep_proghalt" = "1" ] ; then
-        echo "0 19 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
-        else if [ "$rep_proghalt" = "2" ] ; then
-                echo "0 20 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
-                else if [ "$rep_proghalt" = "3" ] ; then
-                        echo "0 22 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
-                     fi
-             fi
+  echo "0 19 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
+elif [ "$rep_proghalt" = "2" ] ; then
+  echo "0 20 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
+elif [ "$rep_proghalt" = "3" ] ; then
+  echo "0 22 * * * root /sbin/shutdown -h now" > /etc/cron.d/prog_extinction
 fi
 
 ########################################################################
@@ -147,22 +143,22 @@ port=$port_proxy
 
   glib-compile-schemas /usr/share/glib-2.0/schemas
 
-#Paramétrage du Proxy pour le systeme
+#Paramétrage du Proxy pour le système
 ######################################################################
-echo "http_proxy=http://$ip_proxy:$port_proxy/
+  echo "http_proxy=http://$ip_proxy:$port_proxy/
 https_proxy=http://$ip_proxy:$port_proxy/
 ftp_proxy=http://$ip_proxy:$port_proxy/
 no_proxy=\"$proxy_env_noproxy\"" >> /etc/environment
 
 #Paramétrage du Proxy pour apt
 ######################################################################
-echo "Acquire::http::proxy \"http://$ip_proxy:$port_proxy/\";
+  echo "Acquire::http::proxy \"http://$ip_proxy:$port_proxy/\";
 Acquire::ftp::proxy \"ftp://$ip_proxy:$port_proxy/\";
 Acquire::https::proxy \"https://$ip_proxy:$port_proxy/\";" > /etc/apt/apt.conf.d/20proxy
 
-#Permettre d'utiliser la commande add-apt-repository derriere un Proxy
+#Permettre d'utiliser la commande add-apt-repository derrière un Proxy
 ######################################################################
-echo "Defaults env_keep = https_proxy" >> /etc/sudoers
+  echo "Defaults env_keep = https_proxy" >> /etc/sudoers
 
 fi
 
@@ -177,8 +173,8 @@ apt-get -y install ntpdate ;
 ntpdate $ip_scribe
 
 ########################################################################
-#installation des paquets necessaires
-#numlockx pour le verrouillage du pave numerique
+#installation des paquets nécessaires
+#numlockx pour le verrouillage du pave numérique
 #unattended-upgrades pour forcer les mises à jour de sécurité à se faire
 ########################################################################
 apt-get install -y ldap-auth-client libpam-mount cifs-utils nscd numlockx unattended-upgrades
@@ -250,7 +246,12 @@ pam-auth-update consolekit ldap libpam-mount unix mkhomedir my_groups --force
 ########################################################################
 # mise en place des groupes pour les users ldap dans /etc/security/group.conf
 ########################################################################
-grep "*;*;*;Al0000-2400;floppy,audio,cdrom,video,plugdev,scanner,dialout" /etc/security/group.conf  >/dev/null; if [ $? != 0 ];then echo "*;*;*;Al0000-2400;floppy,audio,cdrom,video,plugdev,scanner,dialout" >> /etc/security/group.conf; else echo "group.conf ok";fi
+grep "*;*;*;Al0000-2400;floppy,audio,cdrom,video,plugdev,scanner,dialout" /etc/security/group.conf > /dev/null
+if [ $? != 0 ] ; then
+  echo "*;*;*;Al0000-2400;floppy,audio,cdrom,video,plugdev,scanner,dialout" >> /etc/security/group.conf
+else
+  echo "group.conf ok"
+fi
 
 ########################################################################
 #on remet debconf dans sa conf initiale
@@ -259,14 +260,13 @@ export DEBIAN_FRONTEND="dialog"
 export DEBIAN_PRIORITY="high"
 
 ########################################################################
-#parametrage du script de demontage du netlogon pour lightdm # désactivé pour Mint
+#paramétrage du script de démontage du netlogon pour lightdm # désactivé pour Mint
 ########################################################################
-if [ "$DISTRIB_RELEASE" = "14.04" ] || [ "$DISTRIB_RELEASE" = "16.04" ] ; then 
+if [ "$DISTRIB_RELEASE" = "14.04" ] || [ "$DISTRIB_RELEASE" = "16.04" ] ; then
 
 touch /etc/lightdm/logonscript.sh
 grep "if mount | grep -q \"/tmp/netlogon\" ; then umount /tmp/netlogon ;fi" /etc/lightdm/logonscript.sh  >/dev/null
-if [ $? == 0 ]
-then
+if [ $? == 0 ] ; then
   echo "Presession Ok"
 else
   echo "if mount | grep -q \"/tmp/netlogon\" ; then umount /tmp/netlogon ;fi" >> /etc/lightdm/logonscript.sh
@@ -275,14 +275,14 @@ chmod +x /etc/lightdm/logonscript.sh
 
 touch /etc/lightdm/logoffscript.sh
 echo "sleep 2 \
-umount -f /tmp/netlogon \ 
+umount -f /tmp/netlogon \
 umount -f \$HOME
 " > /etc/lightdm/logoffscript.sh
 chmod +x /etc/lightdm/logoffscript.sh
 
 ########################################################################
-#parametrage du lightdm.conf
-#activation du pave numerique par greeter-setup-script=/usr/bin/numlockx on
+#paramétrage du lightdm.conf
+#activation du pave numérique par greeter-setup-script=/usr/bin/numlockx on
 ########################################################################
 echo "[SeatDefaults]
     allow-guest=false
@@ -301,17 +301,18 @@ fi
 if [ "$(which mdm)" = "/usr/sbin/mdm" ] ; then # si MDM est installé (donc Mint)
   apt-get -y purge mintwelcome hexchat pidgin transmission-gtk banshee
   cp /etc/mdm/mdm.conf /etc/mdm/mdm_old.conf #backup du fichier de config de mdm
-  wget --no-check-certificate https://raw.githubusercontent.com/dane-lyon/fichier-de-config/master/mdm.conf ; mv -f mdm.conf /etc/mdm/ ; 
+  wget --no-check-certificate https://raw.githubusercontent.com/dane-lyon/fichier-de-config/master/mdm.conf
+  mv -f mdm.conf /etc/mdm/
 fi
 
 # Spécifique a Ubuntu Mate
 if [ "$(which caja)" = "/usr/bin/caja" ] ; then
-  apt-get -y purge hexchat transmission-gtk ubuntu-mate-welcome cheese pidgin rhythmbox ;
+  apt-get -y purge hexchat transmission-gtk ubuntu-mate-welcome cheese pidgin rhythmbox
 fi
 
 # Spécifique a Lubuntu (lxde)
 if [ "$(which pcmanfm)" = "/usr/bin/pcmanfm" ] ; then
-  apt-get -y purge abiword gnumeric pidgin transmission-gtk sylpheed audacious guvcview ;
+  apt-get -y purge abiword gnumeric pidgin transmission-gtk sylpheed audacious guvcview
 fi
 
 ########################################################################
@@ -320,33 +321,30 @@ fi
 
 eclairng="<volume user=\"*\" fstype=\"cifs\" server=\"$ip_scribe\" path=\"eclairng\" mountpoint=\"/media/Serveur_Scribe\" />"
 grep "/media/Serveur_Scribe" /etc/security/pam_mount.conf.xml  >/dev/null
-if [ $? != 0 ]
-then
+if [ $? != 0 ] ; then
   sed -i "/<\!-- Volume definitions -->/a\ $eclairng" /etc/security/pam_mount.conf.xml
 else
-  echo "eclairng deja present"
+  echo "eclairng déjà présent"
 fi
 
 homes="<volume user=\"*\" fstype=\"cifs\" server=\"$ip_scribe\" path=\"perso\" mountpoint=\"~/Documents\" />"
 grep "mountpoint=\"~\"" /etc/security/pam_mount.conf.xml  >/dev/null
-if [ $? != 0 ]
-then sed -i "/<\!-- Volume definitions -->/a\ $homes" /etc/security/pam_mount.conf.xml
+if [ $? != 0 ] ; then
+  sed -i "/<\!-- Volume definitions -->/a\ $homes" /etc/security/pam_mount.conf.xml
 else
-  echo "homes deja present"
+  echo "homes déjà présent"
 fi
 
 netlogon="<volume user=\"*\" fstype=\"cifs\" server=\"$ip_scribe\" path=\"netlogon\" mountpoint=\"/tmp/netlogon\"  sgrp=\"DomainUsers\" />"
 grep "/tmp/netlogon" /etc/security/pam_mount.conf.xml  >/dev/null
-if [ $? != 0 ]
-then
+if [ $? != 0 ] ; then
   sed -i "/<\!-- Volume definitions -->/a\ $netlogon" /etc/security/pam_mount.conf.xml
 else
-  echo "netlogon deja present"
+  echo "netlogon déjà présent"
 fi
 
 grep "<cifsmount>mount -t cifs //%(SERVER)/%(VOLUME) %(MNTPT) -o \"noexec,nosetuids,mapchars,cifsacl,serverino,nobrl,iocharset=utf8,user=%(USER),uid=%(USERUID)%(before=\\",\\" OPTIONS)\"</cifsmount>" /etc/security/pam_mount.conf.xml  >/dev/null
-if [ $? != 0 ]
-then
+if [ $? != 0 ] ; then
   sed -i "/<\!-- pam_mount parameters: Volume-related -->/a\ <cifsmount>mount -t cifs //%(SERVER)/%(VOLUME) %(MNTPT) -o \"noexec,nosetuids,mapchars,cifsacl,serverino,nobrl,iocharset=utf8,user=%(USER),uid=%(USERUID)%(before=\\",\\" OPTIONS)\"</cifsmount>" /etc/security/pam_mount.conf.xml
 else
   echo "mount.cifs deja present"
@@ -356,13 +354,13 @@ fi
 #/etc/profile
 ########################################################################
 echo "
-export LC_ALL=fr_FR.utf8
-export LANG=fr_FR.utf8
-export LANGUAGE=fr_FR.utf8
+export LC_ALL=fr_FR.UTF-8
+export LANG=fr_FR.UTF-8
+export LANGUAGE=fr_FR.UTF-8
 " >> /etc/profile
 
 ########################################################################
-#ne pas creer les dossiers par defaut dans home
+#ne pas créer les dossiers par défaut dans home
 ########################################################################
 sed -i "s/enabled=True/enabled=False/g" /etc/xdg/user-dirs.conf
 
@@ -370,38 +368,38 @@ sed -i "s/enabled=True/enabled=False/g" /etc/xdg/user-dirs.conf
 # les profs peuvent sudo
 ########################################################################
 grep "%professeurs ALL=(ALL) ALL" /etc/sudoers > /dev/null
-if [ $?!=0 ]
-then
+if [ $? != 0 ] ; then
   sed -i "/%admin ALL=(ALL) ALL/a\%professeurs ALL=(ALL) ALL" /etc/sudoers
   sed -i "/%admin ALL=(ALL) ALL/a\%DomainAdmins ALL=(ALL) ALL" /etc/sudoers
 else
-  echo "prof deja dans sudo"
+  echo "prof déjà dans sudo"
 fi
 
 
 ########################################################################
-#supression de l'applet switch-user pour ne pas voir les derniers connectés # Uniquement pour Ubuntu / Unity
-#paramétrage d'un laucher unity par défaut : nautilus, firefox, libreoffice, calculatrice, editeur de texte et capture d'ecran
+#suppression de l'applet switch-user pour ne pas voir les derniers connectés # Uniquement pour Ubuntu / Unity
+#paramétrage du launcher unity par défaut : nautilus, firefox, libreoffice, calculatrice, éditeur de texte et capture d'écran
 ########################################################################
 if [ "$(which unity)" = "/usr/bin/unity" ] ; then  # si Ubuntu/Unity alors :
 
-echo "[com.canonical.indicator.session]
+  echo "[com.canonical.indicator.session]
 user-show-menu=false
 [org.gnome.desktop.lockdown]
 disable-user-switching=true
 disable-lock-screen=true
 [com.canonical.Unity.Launcher]
-favorites=[ 'nautilus-home.desktop', 'firefox.desktop','libreoffice-startcenter.desktop', 'gcalctool.desktop','gedit.desktop','gnome-screenshot.desktop' ]
+favorites=[ 'nautilus-home.desktop', 'firefox.desktop', 'libreoffice-startcenter.desktop', 'gnome-calculator.desktop', 'gedit.desktop', 'org.gnome.Screenshot.desktop' ]
 " > /usr/share/glib-2.0/schemas/my-defaults.gschema.override
 
-# Suppression de paquet inutile sous Ubuntu/Unity
-apt-get -y purge aisleriot gnome-mahjongg ;
+  glib-compile-schemas /usr/share/glib-2.0/schemas
+
+# Suppression de paquets inutiles sous Ubuntu/Unity
+  apt-get -y purge aisleriot gnome-mahjongg
 
 fi
 
-# Pour être sûr que les paquets suivant (parfois présent) ne sont pas installés :
-apt-get -y purge pidgin transmission-gtk gnome-mines gnome-sudoku blueman abiword gnumeric thunderbird ;
-
+# Pour être sûr que les paquets suivant (parfois présents) ne sont pas installés :
+apt-get -y purge pidgin transmission-gtk gnome-mines gnome-sudoku blueman abiword gnumeric thunderbird
 
 ########################################################################
 #suppression de l'envoi des rapport d'erreurs
@@ -416,7 +414,7 @@ mv /etc/xdg/autostart/nm-applet.desktop /etc/xdg/autostart/nm-applet.old
 ########################################################################
 #suppression du menu messages
 ########################################################################
-apt-get -y purge indicator-messages 
+apt-get -y purge indicator-messages
 
 # Changement page d'accueil firefox
 echo "user_pref(\"browser.startup.homepage\", \"$pagedemarragepardefaut\");" >> /usr/lib/firefox/defaults/pref/channel-prefs.js
