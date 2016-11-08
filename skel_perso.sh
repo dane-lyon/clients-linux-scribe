2844ae20dc4be7800a4bc62336190b90e99f6b67
Simon.B <sibe39@users.noreply.github.com>
2016-11-08 15:14:19 +0100

    Update skel_perso.sh

--- skel_perso.sh
+++ skel_perso.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#test !!
 
 # Ce script est là pour automatiser la copie du profil en cours dans le répertoire skel afin d'avoir un bureau customisé
 #homogène sur l'ensemble du parc. Vous devez donc avoir personnalisé le bureau comme vous le souhaitez avant d'appliquer
