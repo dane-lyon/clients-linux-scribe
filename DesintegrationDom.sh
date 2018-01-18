8e0bbda1a025141fc4cf765557187fc21817533b
gegui69 <32762326+gegui69@users.noreply.github.com>
2018-01-18 13:29:20 +0100

    Update DesintegrationDom.sh

--- DesintegrationDom.sh
+++ DesintegrationDom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #v1.0
 
-# Le but de ce script est de faire l'inverse du script d'intégration au domaine c'est a dire :
+# Le but de ce script est de faire l'inverse du script d'intégration au domaine c'est à dire :
 # Retirer du domaine un Ubuntu (ou variante) qui a été précédemment intégré avec le script d'intégration.
 
 
@@ -28,7 +28,7 @@ sed -i "s/enabled=False/enabled=True/g" /etc/xdg/user-dirs.conf
 
 
 echo "Désintégration terminé"
-read -p "Il est fortement recommandé de redémarrer, voulez vous le faire immédiatement ? [O/n] " rep_reboot
+read -p "Il est fortement recommandé de redémarrer, voulez-vous le faire immédiatement ? [O/n] " rep_reboot
 if [ "$rep_reboot" = "O" ] || [ "$rep_reboot" = "o" ] || [ "$rep_reboot" = "" ] ; then
   reboot
 fi
