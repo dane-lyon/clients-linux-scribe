d706ac3becc48beecacf94df7e981be7c301de96
gegui69 <32762326+gegui69@users.noreply.github.com>
2018-01-18 13:27:14 +0100

    Update Creation_skel_depuis_modele.sh

--- Creation_skel_depuis_modele.sh
+++ Creation_skel_depuis_modele.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 # N'utilisez pas ce script si vous ne savez pas ce que vous faites !
 
-# Avant de lancer ce script : merci de faire le nettoyage en supprimant dans votre cache tous les fichiers/dossiers que vous jugez inutile (geogebra, libreoffice...)
-# Une fois un profil-modèle établis, vous pouvez utiliser ce script pour copier dans /etc/skel pour s'appliquer à l'ensemble des utilisateurs.
+# Avant de lancer ce script : merci de faire le nettoyage en supprimant dans votre cache tous les fichiers/dossiers que vous jugez inutiles (geogebra, libreoffice...)
+# Une fois un profil-modèle établi, vous pouvez utiliser ce script pour copier dans /etc/skel pour s'appliquer à l'ensemble des utilisateurs.
 
 #############################################
 # Run using sudo, of course.
