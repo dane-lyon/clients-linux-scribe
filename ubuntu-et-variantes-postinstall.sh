8031f4c6a0ca2246a1aad1f0462f417a2e183c43
Simon B <sibe39@users.noreply.github.com>
2018-03-01 11:02:40 +0100

    Update ubuntu-et-variantes-postinstall.sh

--- ubuntu-et-variantes-postinstall.sh
+++ ubuntu-et-variantes-postinstall.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-# version 1.0.8
+# version 2.0.0
 
-# Variantes concernées :
-# - Ubuntu 14.04/16.04
-# - Xubuntu 14.04/16.04
-# - Lubuntu 14.04/16.04
-# - Ubuntu Mate 16.04
-# - Linux Mint 17.X/18.X
+# Ce script sert à installer des logiciels supplémentaires utiles pour les collèges & lyçées
+# Ce script est utilisable pour Ubuntu et variantes en 14.04, 16.04 et 18.04
 
 #############################################
 # Run using sudo, of course.
@@ -16,42 +12,45 @@ if [ "$UID" -ne "0" ] ; then
   exit 
 fi 
 
-# Pour identifier le numéro de la version (14.04, 16.04...)
+# Pour identifier le numéro de la version
 . /etc/lsb-release
 
 # Affectation à la variable "version" suivant la variante utilisée
 
-if [ "$DISTRIB_RELEASE" = "14.04" ] || [ "$(echo "$DISTRIB_RELEASE" | cut -c -2)" = "17" ] ; then
+if [ "$DISTRIB_RELEASE" = "14.04" ] ; then
   version=trusty
 fi
 
-if [ "$DISTRIB_RELEASE" = "16.04" ] || [ "$(echo "$DISTRIB_RELEASE" | cut -c -2)" = "18" ] || [ "$(echo "$DISTRIB_RELEASE" | cut -c -3)" = "0.4" ] ; then
+if [ "$DISTRIB_RELEASE" = "16.04" ] || [ "$DISTRIB_RELEASE" = "18.3" ] || [ "$(echo "$DISTRIB_RELEASE" | cut -c -3)" = "0.4" ] ; then
   version=xenial
+fi
+
+if [ "$DISTRIB_RELEASE" = "18.04" ] ; then
+  version=bionic
 fi
 
 ########################################################################
 #vérification de la bonne version d'Ubuntu
 ########################################################################
 
-if [ "$version" != "trusty" ] && [ "$version" != "xenial" ] ; then
-  echo "Vous n'êtes pas sur une version compatible ! Le script est conçu uniquement pour les LTS (non-obsolètes), c'est à dire la 14.04 ou la 16.04"
+if [ "$version" != "trusty" ] && [ "$version" != "xenial" ] && [ "$version" != "bionic" ] ; then
+  echo "Vous n'êtes pas sur une version compatible ! Le script est conçu uniquement pour les LTS (non-obsolètes), c'est à dire la 14.04 ou la 16.04 ou la 18.04"
   exit
 fi
 
 # désactiver mode intéractif pour automatiser l'installation de wireshark
 export DEBIAN_FRONTEND="noninteractive"
 
+# Ajout dépot partenaire
+add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner" 
+
 # Vérification que le système est à jour
 apt-get update ; apt-get -y dist-upgrade
 
 #########################################
-# Paquet uniquement pour Trusty
+# Paquets uniquement pour Trusty (14.04)
 #########################################
 if [ "$version" = "trusty" ] ; then
-  # activation dépot partenaire 
-  if [ "$(which mdm)" != "/usr/sbin/mdm"  ] ; then # activation du dépot partenaire (sauf pour Mint car déjà présent)
-    echo "deb http://archive.canonical.com/ubuntu trusty partner" >> /etc/apt/sources.list
-  fi
 
   # paquet
   apt-get -y install idle-python3.4 gstreamer0.10-plugins-ugly celestia
@@ -65,45 +64,67 @@ if [ "$version" = "trusty" ] ; then
 fi
 
 #########################################
-# Paquet uniquement pour Xenial
+# Paquets uniquement pour Xenial (16.04)
 #########################################
 if [ "$version" = "xenial" ] ; then
 
-  # activation dépot partenaire 
-  if [ "$(which mdm)" != "/usr/sbin/mdm"  ] ; then # activation du dépot partenaire (sauf pour Mint car déjà présent)
-    echo "deb http://archive.canonical.com/ubuntu xenial partner" >> /etc/apt/sources.list
-    apt-get update #pour prendre en compte le dépot 
-  fi
-  
   # Installation style "Breeze" pour LibreOffice si il est n'est pas installé (exemple : Xubuntu 16.04...)
   apt install -y libreoffice-style-breeze ;
   # paquet
   apt install -y idle-python3.5 x265 ;
 
-  # Pour Google Earth (64 bits only) sur Xenial # A décommenter si vous voulez Google Earth
-  #wget --no-check-certificate https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb 
-  #wget http://ftp.fr.debian.org/debian/pool/main/l/lsb/lsb-core_4.1+Debian13+nmu1_amd64.deb
-  #wget http://ftp.fr.debian.org/debian/pool/main/l/lsb/lsb-security_4.1+Debian13+nmu1_amd64.deb 
-  #dpkg -i lsb*.deb
-  #dpkg -i google-earth*.deb
-  #apt-get -fy install 
+  # Pour Google Earth (64 bits only) sur Xenial #a décommenter pour avoir google earth sur une 16.04 !
+  wget --no-check-certificate https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb 
+  wget http://ftp.fr.debian.org/debian/pool/main/l/lsb/lsb-core_4.1+Debian13+nmu1_amd64.deb
+  wget http://ftp.fr.debian.org/debian/pool/main/l/lsb/lsb-security_4.1+Debian13+nmu1_amd64.deb 
+  dpkg -i lsb*.deb
+  dpkg -i google-earth*.deb
+  apt-get -fy install 
   
-  # Pour Celestia X64 # A décommenter si vous voulez Celestia
-  #wget --no-check-certificate https://raw.githubusercontent.com/sibe39/scripts_divers/master/Celestia_On_Xenial.sh
-  #chmod +x Celestia_On_Xenial.sh
-  #./Celestia_On_Xenial.sh
-  #rm -f Celestia_On_Xenial.sh
-
+  # Pour Celestia X64 # A décommenter si vous voulez Celestia pour la 16.04
+  wget --no-check-certificate https://raw.githubusercontent.com/sibe39/scripts_divers/master/Celestia_On_Xenial.sh
+  chmod +x Celestia_On_Xenial.sh ; ./Celestia_On_Xenial.sh ; rm -f Celestia_On_Xenial.sh
 fi
+
+#########################################
+# Paquet uniquement pour Bionic (18.04)
+#########################################
+if [ "$version" = "bionic" ] ; then
+
+  # paquet
+  apt install -y idle-python3.6 x265
+
+  # Backportage LibreOffice
+  add-apt-repository -y ppa:libreoffice/ppa ; apt-get update ; apt-get -y upgrade
+
+  # Google Earth Pro 
+  wget https://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
+  dpkg -i google-earth-pro-stable_current_amd64.deb
+  apt install -fy ; rm google-earth-pro-stable_current_amd64.deb
+  
+  # Celestia
+  wget --no-check-certificate https://raw.githubusercontent.com/BionicBeaver/Divers/master/CelestiaBionic.sh
+  chmod +x CelestiaBionic.sh ; ./CelestiaBionic.sh ; rm CelestiaBionic.sh
+  
+  # Pilote imprimante openprinting
+  apt install -y openprinting-ppds
+fi
+
+
 
 #=======================================================================================================#
 
 # Installation quelque soit la variante et la version 
 
-# drivers imprimantes
-wget http://www.openprinting.org/download/printdriver/debian/dists/lsb3.2/contrib/binary-amd64/openprinting-gutenprint_5.2.7-1lsb3.2_amd64.deb
-dpkg -i openprinting-gutenprint_5.2.7-1lsb3.2_amd64.deb ;
-apt-get -fy install ;
+if [ "$version" != "bionic" ] ; then
+  # drivers imprimantes (sauf pour Bionic ou il est installé différemment)
+  wget http://www.openprinting.org/download/printdriver/debian/dists/lsb3.2/contrib/binary-amd64/openprinting-gutenprint_5.2.7-1lsb3.2_amd64.deb
+  dpkg -i openprinting-gutenprint_5.2.7-1lsb3.2_amd64.deb ; apt-get -fy install ; rm openprinting-gutenprint*
+  
+  # Gdevelop (PPA pas encore actif pour la 18.04)
+  add-apt-repository -y ppa:florian-rival/gdevelop
+  apt-get update ; apt-get -y install gdevelop
+fi
 
 # drivers pour les scanners les plus courants
 
@@ -148,14 +169,18 @@ apt-get -y install stellarium avogadro
 apt-get -y install scratch ghex geany imagemagick gcolor2
 apt-get -y install python3-pil.imagetk python3-pil traceroute python3-tk #python3-sympy
 
-# Gdevelop
-add-apt-repository -y ppa:florian-rival/gdevelop
-apt-get update ; apt-get -y install gdevelop
-
 #[ Serveur ]
 #apt-get -y install openssh-server #à décommenter si vous utilisez "Ansible"
 #=======================================================================================================#
 
+################################
+# Concerne Ubuntu / Gnome
+################################
+if [ "$(which gnome-shell)" = "/usr/bin/gnome-shell" ] ; then  # si GS install
+  #[ Paquet AddOns ]
+  apt install -y ubuntu-restricted-extras ubuntu-restricted-addons gnome-tweak-tool
+  apt install -y nautilus-image-converter nautilus-script-audio-convert
+fi
 
 ################################
 # Concerne Ubuntu / Unity
