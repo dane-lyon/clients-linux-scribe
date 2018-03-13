228c30e68e253fe7df0c460244424eb7ca568f78
Simbd <10208905+simbd@users.noreply.github.com>
2018-03-13 15:59:43 +0100

    Update DesintegrationDom.sh

--- DesintegrationDom.sh
+++ DesintegrationDom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#v1.0
+#v1.1
 
 # Le but de ce script est de faire l'inverse du script d'intégration au domaine c'est à dire :
 # Retirer du domaine un Ubuntu (ou variante) qui a été précédemment intégré avec le script d'intégration.
@@ -25,6 +25,14 @@ rm -f /etc/lightdm/logoffscript.sh
 rm -f /usr/share/lightdm/lightdm.conf.d/50-no-guest.conf
 sed -i "s/enabled=False/enabled=True/g" /etc/xdg/user-dirs.conf
 
+# Uniquement pour le cas ou Esubuntu a été installé 
+apt-get remove -y zenity conky
+rm -rf /usr/local/upkg_client
+rm -f /etc/xdg/autostart/message_scribe.desktop
+rm -f /etc/xdg/autostart/scribe_background.desktop
+rm -f /etc/GM_ESU
+rm -f /etc/lightdm/groupe.sh
+echo "" > /etc/crontab #remplacement du contenu d'esubuntu par du contenu vide
 
 
 echo "Désintégration terminé"
